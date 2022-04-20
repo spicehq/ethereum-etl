1b53ebb2e22590976191a17e37e246e7b9624cc8
Phillip LeBlanc <phillip@leblanc.tech>
2022-04-20 09:03:04 +0900

    Load parquet

--- ethereumetl/cli/insert_tokens.py
+++ ethereumetl/cli/insert_tokens.py
@@ -1,6 +1,10 @@
 import click
 import pandas as pd
 
+from sqlalchemy import create_engine
+from sqlalchemy.dialects.postgresql import insert
+
+from ethereumetl.streaming.postgres_tables import TOKENS
 from blockchainetl.logging_utils import logging_basic_config
 
 logging_basic_config()
@@ -14,4 +18,21 @@ def insert_tokens_from_parquet(token_parquet, postgres_connection_string):
     """Insert ERC20/ERC721 tokens to PostgreSQL from a Parquet file."""
     token_df = pd.read_parquet(token_parquet)
 
-    print(token_df)
+    token_df["block_timestamp"] = (token_df["block_timestamp"].astype(int) / 1e9).astype(int)
+
+    engine = create_engine(postgres_connection_string,
+                               echo=True, pool_recycle=3600)
+    connection = engine.connect()
+
+    insert_stmt = insert(TOKENS)
+
+    insert_stmt = insert_stmt.on_conflict_do_update(
+        index_elements=["address"],
+        set_={
+            "block_number": insert_stmt.excluded["block_number"],
+            "block_timestamp": insert_stmt.excluded["block_timestamp"],
+            "block_hash": insert_stmt.excluded["block_hash"]
+        }
+    )
+
+    connection.execute(insert_stmt, token_df.to_dict('records'))
